1db06fdbf3d64c93b6fa1aaa5ef0489ae1e9c3ef
bindu7772 <manubpatel4@gmail.com>
2017-12-17 20:09:41 -0500

    style

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@ $.getJSON("/recipes", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#recipes").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#recipes").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>"); 
+
   }
 });
 
